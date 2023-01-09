1a03b7d55537faffdb8f55c277d77c1c5b624df9
Hyeeun Shin <as221323@kookmin.ac.kr>
2023-01-09 14:03:55 +0900

    Update 1157.py

--- 1157.py
+++ 1157.py
@@ -1,9 +1,15 @@
 from collections import Counter
 
-question = input()
+question = input().upper()
 
 count = Counter(question)
-print(count.most_common())
+# print(count.most_common())
 
-if count.
-print(count.most_common(1)[0][0].upper())
+if len(count) == 1:
+    print(count.most_common(1)[0][0])
+else:
+    if count.most_common()[0][1] == count.most_common()[1][1]:
+        print("?")
+    else:   
+        print(count.most_common(1)[0][0])
+
