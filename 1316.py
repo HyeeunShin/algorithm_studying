2a5f858cca6499c71696883b53f8319223a2a1b3
Hyeeun Shin <as221323@kookmin.ac.kr>
2023-01-12 13:44:03 +0900

    Update 1316.py

--- 1316.py
+++ 1316.py
@@ -1,25 +1,18 @@
 def GroupWord(word):
 
-    include_txt = list() 
+    txtList = list() 
 
     for i in range(len(word)): 
-        print(i)
-        print("len: ", len(word))
-     
-        if(len(word) > 1):
-            if word[i] not in include_txt: #i번째 단어가 리스트에 없으면
-                include_txt.append(word[i]) #리스트에 추가
-                if word[i] == word[i+1]:
-                    word = word.replace(word[i+1], "", 1)
-            else:
-                return False
-        else:
-            print("whye" , i, word)
+        if word[i] not in txtList:
+            txtList.append(word[i])
+        elif (word[i] in txtList) & (word[i] == word[i-1]):
+            word = word.replace(word[i], " ", 1)
+        elif (word[i] in txtList) & (word[i] != word[i-1]):
+            return False
+    
+    return True
 
-            if word[i-1] not in include_txt:
-                return True
-            else: 
-                return False
+
 
 n = int(input())
 text_list = list()
